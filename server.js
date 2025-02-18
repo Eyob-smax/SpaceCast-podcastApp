8bcaf40d121b2ee6a70a0b431574f5e5e2673ad5
Eyoba_Smax <eyobsmax@gmail.com>
2025-02-18 17:16:49 +0300

    Initializing basic react server

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
+
+const app = express();
+dotenv.config();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", async (req, res) => {});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
