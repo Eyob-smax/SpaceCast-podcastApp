82b2aa63717f777799bbca9c915fdfb962d4904a
Eyoba_Smax <eyobsmax@gmail.com>
2025-02-18 17:21:27 +0300

    Initializing basic express server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
+import crypto from "crypto";
+import cors from "cors";
+import path from "path";
 
 const app = express();
 dotenv.config();
@@ -8,7 +11,9 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", async (req, res) => {});
+app.get("/", async (req, res) => {
+  res.send("API is running...");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
