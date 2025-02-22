48d0dfafcdb10611235f409fed18ca4de2e6af56
Eyoba_Smax <eyobsmax@gmail.com>
2025-02-23 02:40:13 +0300

    editing the port

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,12 @@
 const cacheName = "podcast-v1";
 const filesToCache = [
-  "/public/manifest.json",
-  "/index.html",
-  "/public/style.css",
-  "/public/script.js",
-  "/public/Podcast_01_generated.jpg",
-  "/public/Podcast-icon-192.png",
-  "/public/Podcast-icon-512.png",
+  "manifest.json",
+  "index.html",
+  "style.css",
+  "script.js",
+  "Podcast_01_generated.jpg",
+  "Podcast-icon-192.png",
+  "Podcast-icon-512.png",
 ];
 
 self.addEventListener("install", (event) => {
