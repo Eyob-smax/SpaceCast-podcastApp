6bbca7546a013bfaf4e4e858f4cc6bb666ebe87c
Eyoba_Smax <eyobsmax@gmail.com>
2025-02-23 14:45:39 +0300

    finalizing

--- public/script.js
+++ public/script.js
@@ -884,16 +884,16 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-      navigator.serviceWorker
-        .register("./service-worker.js")
-        .then((registration) => {
-          console.log("Service worker registered", registration);
-        })
-        .catch((error) => {
-          console.log("Service worker registration failed", error.message);
-        });
-    });
-  }
+  // if ("serviceWorker" in navigator) {
+  //   window.addEventListener("load", () => {
+  //     navigator.serviceWorker
+  //       .register("./service-worker.js")
+  //       .then((registration) => {
+  //         console.log("Service worker registered", registration);
+  //       })
+  //       .catch((error) => {
+  //         console.log("Service worker registration failed", error.message);
+  //       });
+  //   });
+  // }
 });
